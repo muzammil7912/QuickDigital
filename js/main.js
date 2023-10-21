0fb2b4617abbfc5e7bd568d9cd123079cfc180d9
Muzammil React(Developer) <79418859+muzammil7912@users.noreply.github.com>
2023-10-21 17:35:17 +0500

    mu

--- js/main.js
+++ js/main.js
@@ -71,65 +71,54 @@ var swiper3 = new Swiper(".mySwiper3", {
         },
     },
 });
+var swiper4 = new Swiper(".mySwiper4", {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 1,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 1,
+            spaceBetween: 50,
+        },
+    },
+});
+
+var swiper5 = new Swiper(".section5MainSwiper", {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 1,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 1,
+            spaceBetween: 50,
+        },
+    },
+});
 
 function ResponsiveFunction() {
-    $(".mySwiper4").addClass("swiper");
-    $(".swiper-wrapper2").addClass("swiper-wrapper");
-    $(".swiper-wrapp").addClass("swiper-wrapper");
-    $(".swiper-slide3").addClass("swiper-slide");
-    $(".swiper-slidetes").addClass("swiper-slide");
 
-
-    var swiper4 = new Swiper(".mySwiper4", {
-        loop: true,
-        slidesPerView: 1,
-        spaceBetween: 10,
-
-        breakpoints: {
-            640: {
-                slidesPerView: 1,
-                spaceBetween: 20,
-            },
-            768: {
-                slidesPerView: 1,
-                spaceBetween: 40,
-            },
-            1024: {
-                slidesPerView: 1,
-                spaceBetween: 50,
-            },
-        },
-    });
-    $(".swiper-slidetes:nth-child(1)").remove();
-    $(".swiper-slidetes:last-child").remove();
-    var swiper5 = new Swiper(".section5Main", {
-        loop: true,
-        slidesPerView: 1,
-        spaceBetween: 10,
-
-        breakpoints: {
-            640: {
-                slidesPerView: 1,
-                spaceBetween: 20,
-            },
-            768: {
-                slidesPerView: 1,
-                spaceBetween: 40,
-            },
-            1024: {
-                slidesPerView: 1,
-                spaceBetween: 50,
-            },
-        },
-    });
 
 
 }
 
-
-if (screen.width < 979) {
-    ResponsiveFunction()
-}
 
 
 
@@ -138,4 +127,8 @@ $(".header__right-nav").click(function() {
 })
 $(document).on("click", ".closeIcon", function() {
     $(".header__right").removeClass("active");
+    $(".header__right li").removeClass("active");
+});
+$(document).on("click", ".header__right li", function() {
+    $(this).addClass("active");
 });
