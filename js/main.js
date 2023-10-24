26a2f1146938495bc4fef8183240b34f5f86746b
Muzammil React(Developer) <79418859+muzammil7912@users.noreply.github.com>
2023-10-24 18:54:02 +0500

    mu

--- js/main.js
+++ js/main.js
@@ -182,3 +182,78 @@ $(window).scroll(function() {
     //}
 });
 // counter
+
+
+// Define smooth scrolling animation using GSAP
+
+// smooth scroll start
+// $(document).ready(function() {
+//     jQuery.scrollSpeed(200, 2000);
+// });
+// (function($) {
+//     jQuery.scrollSpeed = function(step, speed, easing) {
+//         var $document = $(document),
+//             $window = $(window),
+//             $body = $('html, body'),
+//             option = easing || 'default',
+//             root = 0,
+//             scroll = false,
+//             scrollY, scrollX, view;
+//         if (window.navigator.msPointerEnabled) return false;
+//         $window.on('mousewheel DOMMouseScroll', function(e) {
+//             var deltaY = e.originalEvent.wheelDeltaY,
+//                 detail = e.originalEvent.detail;
+//             scrollY = $document.height() > $window.height();
+//             scrollX = $document.width() > $window.width();
+//             scroll = true;
+//             if (scrollY) {
+//                 view = $window.height();
+//                 if (deltaY < 0 || detail > 0) root = (root + view) >= $document.height() ? root : root += step;
+//                 if (deltaY > 0 || detail < 0) root = root <= 0 ? 0 : root -= step;
+//                 $body.stop().animate({
+//                     scrollTop: root
+//                 }, speed, option, function() {
+//                     scroll = false;
+//                 });
+//             }
+//             if (scrollX) {
+//                 view = $window.width();
+//                 if (deltaY < 0 || detail > 0) root = (root + view) >= $document.width() ? root : root += step;
+//                 if (deltaY > 0 || detail < 0) root = root <= 0 ? 0 : root -= step;
+//                 $body.stop().animate({
+//                     scrollLeft: root
+//                 }, speed, option, function() {
+//                     scroll = false;
+//                 });
+//             }
+//             return false;
+//         }).on('scroll', function() {
+//             if (scrollY && !scroll) root = $window.scrollTop();
+//             if (scrollX && !scroll) root = $window.scrollLeft();
+//         }).on('resize', function() {
+//             if (scrollY && !scroll) view = $window.height();
+//             if (scrollX && !scroll) view = $window.width();
+//         });
+//     };
+//     jQuery.easing.default = function(x, t, b, c, d) {
+//         return -c * ((t = t / d - 1) * t * t * t - 1) + b;
+//     };
+// })(jQuery);
+
+
+$(".section11_2_text_box2").click(function() {
+    $(this).siblings(".section11_2_box_bottom").slideToggle();
+})
+
+
+$(window).on('scroll', function() {
+    if ($(this).scrollTop() > 100) {
+        $('.header').addClass('go-top');
+        // $('.logo_side').addClass('hide');
+        // $('.logo_side_2').removeClass('hide');
+    } else {
+        $('.header').removeClass('go-top');
+        // $('.logo_side').removeClass('hide');
+        // $('.logo_side_2').addClass('hide');
+    }
+});
