54e9e2ca2969fdd6b40331d013894737922bf096
Muzammil React(Developer) <79418859+muzammil7912@users.noreply.github.com>
2023-10-30 09:54:07 +0500

    mu

--- js/main.js
+++ js/main.js
@@ -145,15 +145,18 @@ function ResponsiveFunction() {
 
 
 
-$(".header__right-nav").click(function () {
+$(".header__right-nav").click(function() {
     $(".header__right").toggleClass("active");
 })
-$(document).on("click", ".closeIcon", function () {
+$(document).on("click", ".closeIcon", function() {
     $(".header__right").removeClass("active");
     $(".header__right li").removeClass("active");
+    $(".servicesMensu").removeClass("active");
 });
-$(document).on("click", ".header__right li", function () {
-    $(this).addClass("active");
+$(document).on("click", ".header__right  i", function() {
+    $(this).parent("li").toggleClass("active");
+    $(".servicesMensu").toggleClass("active");
+
 });
 
 
@@ -184,10 +187,10 @@ if (language) {
 
 // counter
 var a = 0;
-$(window).scroll(function () {
+$(window).scroll(function() {
     //var oTop = $('#counter').offset().top - window.innerHeight;
     // if (a == 0 && $(window).scrollTop() > oTop) {
-    $('.counter-value').each(function () {
+    $('.counter-value').each(function() {
         var $this = $(this),
             countTo = $this.attr('data-count');
         $({
@@ -197,10 +200,10 @@ $(window).scroll(function () {
         }, {
             duration: 2000,
             easing: 'swing',
-            step: function () {
+            step: function() {
                 $this.text(Math.floor(this.countNum));
             },
-            complete: function () {
+            complete: function() {
                 $this.text(this.countNum);
                 //alert('finished');
             }
@@ -209,72 +212,15 @@ $(window).scroll(function () {
     a = 1;
     //}
 });
-// counter
 
 
-// Define smooth scrolling animation using GSAP
 
-// smooth scroll start
-// $(document).ready(function() {
-//     jQuery.scrollSpeed(200, 2000);
-// });
-// (function($) {
-//     jQuery.scrollSpeed = function(step, speed, easing) {
-//         var $document = $(document),
-//             $window = $(window),
-//             $body = $('html, body'),
-//             option = easing || 'default',
-//             root = 0,
-//             scroll = false,
-//             scrollY, scrollX, view;
-//         if (window.navigator.msPointerEnabled) return false;
-//         $window.on('mousewheel DOMMouseScroll', function(e) {
-//             var deltaY = e.originalEvent.wheelDeltaY,
-//                 detail = e.originalEvent.detail;
-//             scrollY = $document.height() > $window.height();
-//             scrollX = $document.width() > $window.width();
-//             scroll = true;
-//             if (scrollY) {
-//                 view = $window.height();
-//                 if (deltaY < 0 || detail > 0) root = (root + view) >= $document.height() ? root : root += step;
-//                 if (deltaY > 0 || detail < 0) root = root <= 0 ? 0 : root -= step;
-//                 $body.stop().animate({
-//                     scrollTop: root
-//                 }, speed, option, function() {
-//                     scroll = false;
-//                 });
-//             }
-//             if (scrollX) {
-//                 view = $window.width();
-//                 if (deltaY < 0 || detail > 0) root = (root + view) >= $document.width() ? root : root += step;
-//                 if (deltaY > 0 || detail < 0) root = root <= 0 ? 0 : root -= step;
-//                 $body.stop().animate({
-//                     scrollLeft: root
-//                 }, speed, option, function() {
-//                     scroll = false;
-//                 });
-//             }
-//             return false;
-//         }).on('scroll', function() {
-//             if (scrollY && !scroll) root = $window.scrollTop();
-//             if (scrollX && !scroll) root = $window.scrollLeft();
-//         }).on('resize', function() {
-//             if (scrollY && !scroll) view = $window.height();
-//             if (scrollX && !scroll) view = $window.width();
-//         });
-//     };
-//     jQuery.easing.default = function(x, t, b, c, d) {
-//         return -c * ((t = t / d - 1) * t * t * t - 1) + b;
-//     };
-// })(jQuery);
-
-
-$(".section11_2_text_box2").click(function () {
+$(".section11_2_text_box2").click(function() {
     $(this).siblings(".section11_2_box_bottom").slideToggle();
 })
 
 
-$(window).on('scroll', function () {
+$(window).on('scroll', function() {
     if ($(this).scrollTop() > 100) {
         $('.header').addClass('go-top');
         // $('.logo_side').addClass('hide');
